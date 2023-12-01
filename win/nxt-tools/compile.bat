8347a2eda95dba0d0ae806bfafefe4fd5a70cf1a
zhitm <zhtinuhina.maria@gmail.com>
2023-12-01 15:54:46 +0300

    added file coping

--- win/nxt-tools/compile.bat
+++ win/nxt-tools/compile.bat
@@ -4,6 +4,8 @@ set B=%~dp0\mingw64\bin\bash.exe
 %B% -c "echo OK" > NUL || goto ERROR_NOBASH
 for %%I in (%B%) do set X=%%~dpI
 set PATH=%X%;%PATH%
+COPY "%cd%\kernel_id.h" "%2"
+COPY "%cd%\kernel_cfg.c" "%2"
 bash -c "source ./compile.sh '%1' '%2' '%cd%' GNUARM_ROOT='%3'"
 goto NOERROR
 :ERROR_NOBASH
